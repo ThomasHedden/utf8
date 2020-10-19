f0881e68a3567428bf6cea14f1466017dbabb3ec
Thomas Hedden <43582071+ThomasHedden@users.noreply.github.com>
2020-10-18 22:19:45 -0400

    Uploaded updated file
    Added test of validity of UTF-8 character before outputting it

--- putu.c
+++ putu.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 /***************************************************************
 *                      NEEDED ENHANCEMENTS                     *
 ***************************************************************/
-/* Need to test for validity of character before outputting it
-*/
 
 /***************************************************************
 *                     FUNCTION DECLARATIONS                    *
 ***************************************************************/
+bool is1butf8(unsigned int);
+bool is2butf8(unsigned int);
+bool is3butf8(unsigned int);
+bool is4butf8(unsigned int);
 
 /***************************************************************
 *                     FUNCTION DEFINITIONS                    *
 ***************************************************************/
 void putu(unsigned int u) {
-   fprintf(stdout, "%c%c%c%c", (char) ((u & 0xFF000000) >> 24),
-                               (char) ((u & 0x00FF0000) >> 16),
-                               (char) ((u & 0x0000FF00) >>  8),
-                               (char) ((u & 0x000000FF)      ));
+   if( is1butf8(u) || is2butf8(u) || is3butf8(u) || is4butf8(u) ) {
+      fprintf(stdout, "%c%c%c%c", (char) ((u & 0xFF000000) >> 24),
+                                  (char) ((u & 0x00FF0000) >> 16),
+                                  (char) ((u & 0x0000FF00) >>  8),
+                                  (char) ((u & 0x000000FF)      ));
+   }
 }
 
