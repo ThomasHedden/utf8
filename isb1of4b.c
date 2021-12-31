5e5ac0b3ef87bce56bc5ca9f031e0ae21f88a112
Thomas Hedden <thomas@hedden.org>
2021-12-30 20:17:07 -0500

    added progs in which this is used

--- isb1of4b.c
+++ isb1of4b.c
@@ -11,7 +11,7 @@ Post:           This function returns boolean true if this
                 is false.
 Functions used: only standard library functions
 Includes:       stdio.h, stdbool.h, & stdlib.h (for exit())
-Used in:        main()                                         */
+Used in:        fgetu.c, getu.c, is4butf8.c, isb1ofu8.c  */
 bool isb1of4b(unsigned int u) {
    // this program requires that the size of an int be 4 bytes
    if( sizeof(int) != 4 ) { 
@@ -24,7 +24,5 @@ bool isb1of4b(unsigned int u) {
       return false;
    }
 }; /* end isb1of4b() */
-
-
 
 
