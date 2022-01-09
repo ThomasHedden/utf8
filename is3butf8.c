53f81b836b735ca152b5fcce60884c6d1518c9f3
Thomas Hedden <thomas@hedden.org>
2022-01-09 09:16:04 -0500

    improved documenting comments

--- is3butf8.c
+++ is3butf8.c
@@ -11,13 +11,12 @@ is3butf8(unsigned int)
 Pre:            This function takes an unsigned int.
 Post:           This function returns boolean true if this
                 unsigned int represents a three-byte UTF-8
-	       	character; otherwise, it returns false.
+                character; otherwise, it returns false.
 Functions used: standard library and isb1of3b() istbutf8()
 Includes:       stdio.h, stdbool.h, & stdlib.h (for exit())
 Used in:        fputu.c, isspaceu.c, putuchar.c, toloweru.c,
                 toupperu.c                                */
 
-// bool isb1of3b(unsigned int);
 bool istbutf8(unsigned int);
 
 bool is3butf8(unsigned int u) {
@@ -34,12 +33,24 @@ bool is3butf8(unsigned int u) {
    unsigned int byte3 = (u & 0x0000FF00) >>  8; // byte 3 of passed unsigned int
    unsigned int byte4 =  u & 0x000000FF;        // byte 4 of passed unsigned int
 
+   /* Trailing bytes check has to account for the following exceptions:
+   if leading byte is 0xE0, range for 1st trailing byte is NOT ...
+      if( byte3 >= 0x00000080 && byte3 <= 0x000000BF ) {
+   ..., but rather ...
+      if( byte3 >= 0x000000A0 && byte3 <= 0x000000BF ) {
+   if leading byte is 0xED, range for 1st trailing byte is NOT ...
+      if( byte3 >= 0x00000080 && byte3 <= 0x000000BF ) {
+   ..., but rather ...
+      if( byte3 >= 0x00000080 && byte3 <= 0x0000009F ) {
+   See Table 3-7. Well-Formed UTF-8 Byte Sequences
+   in Unicode standard
+   So, isb1of3b(byte2) cannot be used to check the leading byte,
+   and istbutf8(byte3) cannot always be used to check the first
+   trailing byte (byte3), because valid range for 1st trailing byte
+   (byte3) depends on the value of the leading byte (byte2). */
+
    // check whether all bytes fall in appropriate ranges
    if((byte1 == 0x00)  && // byte 1 must be zero
-      // can't use isb1of3b(byte2) because valid range
-      // for next byte (byte3) depends on value of byte2
-      // See Table 3-7. Well-Formed UTF-8 Byte Sequences
-      // in Unicode standard
       ( ((byte2 == 0xE0) && ((byte3 >= 0xA0)  && (byte3 <= 0xBF))) ||
        (((byte2 >= 0xE1) &&  (byte2 <= 0xEC)) && istbutf8(byte3))  ||
         ((byte2 == 0xED) && ((byte3 >= 0x80)  && (byte3 <= 0x9F))) ||
@@ -52,14 +63,3 @@ bool is3butf8(unsigned int u) {
    }
 }; /* end is3butf8() */
 
-
-/* Trailing bytes check has to account for the following exceptions:
-if byte1 is E0, range for byte2 (1st trailing byte is NOT ...
-   if( byte2 >= 0x00000080 && byte2 <= 0x000000BF ) {
-..., but rather ...
-   if( byte2 >= 0x000000A0 && byte2 <= 0x000000BF ) {
-if byte1 is ED, range for byte2 (1st trailing byte is NOT ...
-   if( byte2 >= 0x00000080 && byte2 <= 0x000000BF ) {
-..., but rather ...
-   if( byte2 >= 0x00000080 && byte2 <= 0x0000009F ) {
-*/
