01e8c3ac02a9ce5cd830e9f5f45e815b9d601d58
Thomas Hedden <thomas@hedden.org>
2022-01-08 19:05:57 -0500

    Limited -3-byte ranges per Table 3-7

--- is3butf8.c
+++ is3butf8.c
@@ -10,14 +10,14 @@
 is3butf8(unsigned int)
 Pre:            This function takes an unsigned int.
 Post:           This function returns boolean true if this
-                unsigned int is a three-byte UTF-8 character;
-                otherwise, the return value is false.
+                unsigned int represents a three-byte UTF-8
+	       	character; otherwise, it returns false.
 Functions used: standard library and isb1of3b() istbutf8()
 Includes:       stdio.h, stdbool.h, & stdlib.h (for exit())
 Used in:        fputu.c, isspaceu.c, putuchar.c, toloweru.c,
                 toupperu.c                                */
 
-bool isb1of3b(unsigned int);
+// bool isb1of3b(unsigned int);
 bool istbutf8(unsigned int);
 
 bool is3butf8(unsigned int u) {
@@ -27,7 +27,8 @@ bool is3butf8(unsigned int u) {
       exit(EXIT_FAILURE);
    }
 
-   // byte 1 is the (highest-order) leftmost byte
+   // byte 1 is the (highest-order) leftmost byte, so
+   // here the first byte of the 3-byte sequence is byte 2
    unsigned int byte1 = (u & 0xFF000000) >> 24; // byte 1 of passed unsigned int 
    unsigned int byte2 = (u & 0x00FF0000) >> 16; // byte 2 of passed unsigned int
    unsigned int byte3 = (u & 0x0000FF00) >>  8; // byte 3 of passed unsigned int
@@ -35,9 +36,15 @@ bool is3butf8(unsigned int u) {
 
    // check whether all bytes fall in appropriate ranges
    if((byte1 == 0x00)  && // byte 1 must be zero
-      isb1of3b(byte2)  && // range for byte 1 of 3-byte
-      istbutf8(byte3)  && // range for trailing byte
-      istbutf8(byte4)) {  // range for trailing byte
+      // can't use isb1of3b(byte2) because valid range
+      // for next byte (byte3) depends on value of byte2
+      // See Table 3-7. Well-Formed UTF-8 Byte Sequences
+      // in Unicode standard
+      ( ((byte2 == 0xE0) && ((byte3 >= 0xA0)  && (byte3 <= 0xBF))) ||
+       (((byte2 >= 0xE1) &&  (byte2 <= 0xEC)) && istbutf8(byte3))  ||
+        ((byte2 == 0xED) && ((byte3 >= 0x80)  && (byte3 <= 0x9F))) ||
+       (((byte2 >= 0xEE) &&  (byte2 <= 0xEF)) && istbutf8(byte3))) &&
+      istbutf8(byte4) ) {  // byte4 is in range for trailing byte
       return true;
    } else {
       // not all bytes fall in appropriate ranges
@@ -45,3 +52,14 @@ bool is3butf8(unsigned int u) {
    }
 }; /* end is3butf8() */
 
+
+/* Trailing bytes check has to account for the following exceptions:
+if byte1 is E0, range for byte2 (1st trailing byte is NOT ...
+   if( byte2 >= 0x00000080 && byte2 <= 0x000000BF ) {
+..., but rather ...
+   if( byte2 >= 0x000000A0 && byte2 <= 0x000000BF ) {
+if byte1 is ED, range for byte2 (1st trailing byte is NOT ...
+   if( byte2 >= 0x00000080 && byte2 <= 0x000000BF ) {
+..., but rather ...
+   if( byte2 >= 0x00000080 && byte2 <= 0x0000009F ) {
+*/
