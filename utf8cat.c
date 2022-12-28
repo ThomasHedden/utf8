969aae4898742849ee976eb0f1c3762ce6b22f0e
Thomas Hedden <thomas@hedden.org>
2022-12-28 14:56:57 -0500

    New file modeled on strcat()

---
+++ utf8cat.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdbool.h> // needed for is1butf8(), etc.
+#include <stdlib.h>  // needed for exit()
+#include <string.h>  // needed for strlen()
+
+/***************************************************************
+utf8cat(char *, unsigned int) {
+This function takes an unsigned int representing a UTF-8
+character (the 2nd parameter) and appends the UTF-8 character
+to the end of the string that is referenced by the char *
+parameter (the 1st parameter). The first byte of the UTF-8
+character overwrites the NULL character at the end of the
+string.
+It returns a pointer to the string to which the UTF-8
+character has been appended.
+It is the responsibility of the calling function to ensure
+that the string has been allocated enough memory to allow
+the UTF-8 character to be appended to the string.
+Pre:            a NULL-terminated string and an unsigned int
+                representing a UTF-8 character to be appended
+		to the string.
+Post:           the unsigned int representing the UTF-8
+                character is appended to the end of the string,
+		overwriting the NULL character at the end of
+		the string. The function returns the pointer
+		to this string.
+Functions used: standard library functions and is1butf8(), etc.
+Includes:       stdio.h, stdbool.h, stdlib.h, string.h
+Used in:        main(); ()                                   */
+
+/***************************************************************
+*                     FUNCTION DECLARATIONS                    *
+***************************************************************/
+bool is1butf8(unsigned int);
+bool is2butf8(unsigned int);
+bool is3butf8(unsigned int);
+bool is4butf8(unsigned int);
+
+/***************************************************************
+*                     FUNCTION DEFINITIONS                    *
+***************************************************************/
+char * utf8cat(char * str, unsigned int u) {
+   // this program requires that the size of an int be 4 bytes
+   if( sizeof(int) != 4 ) { 
+      fprintf(stderr, "sizeof(int) is not 4!\n");
+      exit(EXIT_FAILURE);
+   }
+   // find end of string
+   unsigned int end = strlen(str);
+   if( is1butf8(u) ) {
+      str[end++] = (char) (u & 0x000000FF);
+      str[end] = '\0';
+      return(str);
+   }
+   if( is2butf8(u) ) {
+      str[end++] = (char) ((u & 0x0000FF00) >>  8),
+      str[end++] = (char) ((u & 0x000000FF)      );
+      str[end] = '\0';
+      return(str);
+   }
+   if( is3butf8(u) ) {
+      str[end++] = (char) ((u & 0x00FF0000) >> 16),
+      str[end++] = (char) ((u & 0x0000FF00) >>  8),
+      str[end++] = (char) ((u & 0x000000FF)      );
+      str[end] = '\0';
+      return(str);
+   }
+   if( is4butf8(u) ) {
+      str[end++] = (char) ((u & 0xFF000000) >> 24),
+      str[end++] = (char) ((u & 0x00FF0000) >> 16),
+      str[end++] = (char) ((u & 0x0000FF00) >>  8),
+      str[end++] = (char) ((u & 0x000000FF)      );
+      str[end] = '\0';
+      return(str);
+   }
+   fprintf(stderr, "0x%X is not a UTF-8 character\n", u);
+}
+
